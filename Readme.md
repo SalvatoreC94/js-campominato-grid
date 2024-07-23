2066c453eb3900108f52eaad8cf8bca00ed58172
SalvatoreC94 <salvatorecozzolino.1594@gmail.com>
2024-07-23 10:11:19 +0200

    esercizio base

--- Readme.md
+++
@@ -1,1 +0,0 @@
-aggiungi Readme
--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('Js Cololegato')
+const startButton = document.getElementById('generate-grid');
+
+startButton.addEventListener('click', function () {
+    const gridContainer = document.getElementById('grid-container');
+    gridContainer.innerHTML = '';
+
+    for (let i = 1; i <= 100; i++) {
+        const newCell = document.createElement('div');
+        newCell.innerHTML = i;
+
+        newCell.addEventListener('click', function () {
+            this.classList.add('clicked');
+            console.log(this.innerHTML);
+        });
+
+        gridContainer.append(newCell);
+    }
+});
